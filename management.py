97c4de6feb6a6aeb21c2b8f776f3b87e80b2b1eb
Sean Snyder <sean@snyderitis.com>
2020-06-30 16:16:09 -0700

    refactor some managment functions to rebuild and cleanup entries

--- management.py
+++ management.py
@@ -1,106 +1,96 @@
-import sys;
-
-sys.path.insert(0, "./venv/lib/python3.8/site-packages")
-
-from botocore.exceptions import ClientError
-import requests
 import boto3
 import time
 import datetime
 import dateutil.tz
-import json
 import os
-from urllib.parse import unquote
-from algoliasearch.search_client import SearchClient
+import helper
 
-def rebuildSearchIndex(event, context):
+
+def rebuild_search_index(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(os.environ['DYANMODB_TABLE'])
 
-    credentials = boto3.Session().get_credentials()
-
-    algoliaClient = SearchClient.create(os.environ['ALGOLIA_APP_ID'], os.environ['ALGOLIA_APP_KEY'])
-    algoliaIndex = algoliaClient.init_index(os.environ['ALGOLIA_INDEX_NAME'])
+    algolia_index = helper.get_algolia_client()
 
     pacific = dateutil.tz.gettz('US/Pacific')
-    pacificDate = datetime.datetime.now(tz=pacific)
-    currentYear = int(pacificDate.strftime("%Y"))
-    currentMonth = int(pacificDate.strftime("%m"))
-    currentDay = int(pacificDate.strftime("%d"))
+    pacific_date = datetime.datetime.now(tz=pacific)
+    current_year = int(pacific_date.strftime("%Y"))
+    current_month = int(pacific_date.strftime("%m"))
+    current_day = int(pacific_date.strftime("%d"))
 
-    dateCursor = datetime.datetime(2005, 1, 1)
-    endDate = datetime.datetime(currentYear, currentMonth, currentDay)
+    date_cursor = datetime.datetime(2005, 1, 1)
+    end_date = datetime.datetime(current_year, current_month, current_day)
 
-    dateCursorStep = datetime.timedelta(days=1)
+    date_cursor_step = datetime.timedelta(days=1)
 
-    while dateCursor <= endDate:
-        dateKey = int(dateCursor.strftime('%Y%m%d'))
-        prettyDate = dateCursor.strftime('%A %B %d %Y')
-        timestamp = time.mktime(dateCursor.timetuple())
+    while date_cursor <= end_date:
+        date_key = int(date_cursor.strftime('%Y%m%d'))
+        pretty_date = date_cursor.strftime('%A %B %d %Y')
+        timestamp = time.mktime(date_cursor.timetuple())
 
-        # Try to get an item by that dateKey
-        response = table.get_item(Key={'date': dateKey})
+        # Try to get an item by that date_key
+        response = table.get_item(Key={'date': date_key})
 
         if 'Item' in response.keys():
-
             # Add other things we wanna search on...
             body = {
-                "objectID": str(dateKey),
+                "objectID": str(date_key),
                 "date": timestamp,
-                "prettyDate": prettyDate,
+                "pretty_date": pretty_date,
                 "entries": response['Item']['entries']
             }
 
-            res = algoliaIndex.save_objects([body])
+            algolia_index.save_objects([body])
 
-            print("rebuilt index for entry: " + str(dateKey))
+            print("rebuilt index for entry: " + str(date_key))
 
-        dateCursor += dateCursorStep
+        date_cursor += date_cursor_step
 
     return True
 
-def cleanupEntries(event, context):
+
+def cleanup_entries(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(os.environ['DYANMODB_TABLE'])
 
     pacific = dateutil.tz.gettz('US/Pacific')
-    pacificDate = datetime.datetime.now(tz=pacific)
-    currentYear = int(pacificDate.strftime("%Y"))
-    currentMonth = int(pacificDate.strftime("%m"))
-    currentDay = int(pacificDate.strftime("%d"))
+    pacific_date = datetime.datetime.now(tz=pacific)
+    current_year = int(pacific_date.strftime("%Y"))
+    current_month = int(pacific_date.strftime("%m"))
+    current_day = int(pacific_date.strftime("%d"))
 
-    dateCursor = datetime.datetime(2005, 1, 1)
-    endDate = datetime.datetime(currentYear, currentMonth, currentDay)
+    date_cursor = datetime.datetime(2005, 1, 1)
+    end_date = datetime.datetime(current_year, current_month, current_day)
 
-    dateCursorStep = datetime.timedelta(days=1)
+    date_cursor_step = datetime.timedelta(days=1)
 
-    while dateCursor <= endDate:
-        dateKey = int(dateCursor.strftime('%Y%m%d'))
+    while date_cursor <= end_date:
+        date_key = int(date_cursor.strftime('%Y%m%d'))
 
-        # Try to get an item by that dateKey
-        response = table.get_item(Key={'date': dateKey})
+        # Try to get an item by that date_key
+        response = table.get_item(Key={'date': date_key})
 
         if 'Item' in response.keys():
             entries = response['Item']['entries']
 
             for key, entry in enumerate(response['Item']['entries']):
-                cleanedEntry = entry
+                cleaned_entry = entry
 
-                cleanedEntry = cleanedEntry.replace("=E2=80=99", "'")
-                cleanedEntry = cleanedEntry.replace("= ", "")
+                cleaned_entry = cleaned_entry.replace("=E2=80=99", "'")
+                cleaned_entry = cleaned_entry.replace("= ", "")
 
-                entries[key] = cleanedEntry
+                entries[key] = cleaned_entry
 
             table.update_item(
-                Key={'date': dateKey},
+                Key={'date': date_key},
                 UpdateExpression='SET entries = :entries',
                 ExpressionAttributeValues={
                     ':entries': entries
                 }
             )
 
-            print("Cleaned up: " + str(dateKey))
+            print("Cleaned up: " + str(date_key))
 
-        dateCursor += dateCursorStep
+        date_cursor += date_cursor_step
 
     return True
